2e22730825e614a28b050b822d5507f47284383c
vangaru shanmukha <vangarushanmukha@gmail.com>
2020-04-13 08:28:45 +0530

    completed about.html

---
+++ script.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("btn");
+window.onscroll = function() {
+    scrollFunction()
+};
+function scrollFunction() {
+    if(document.body.scrollTop > 0) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+function navigate() {
+    document.body.scrollTop = 0;
+}
